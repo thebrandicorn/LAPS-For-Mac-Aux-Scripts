99779ad31d90454619dcb9ad44203c7bdeaddfb2
Brandon Bramstedt <37114834+thebrandicorn@users.noreply.github.com>
2019-06-29 18:47:03 -0500

    Add files via upload

---
+++ Convert admin account to lapsadmin.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+
+#Gets current admin username
+currentadmin="$4"
+
+# Uses dscl to change the account shortname to lapsadmin. 
+# This maintains the home folder, and fullname, so if your admin account is also your management account, Jamf should be able to maintain access
+dscl . change /Users/$currentadmin RecordName $currentadmin lapsadmin
+
+#Give lapsadmin admin access, just in case something went wrong
+dscl . -append /groups/admin GroupMembership lapsadmin
---
+++ Set Lapsadmin to Temp Password.sh
@@ -0,0 +1,28 @@
+﻿#!/bin/bash
+
+resetUser='lapsadmin'
+oldPass="$4"
+newPass="LAPST3mpP4s5word!"
+
+passwdA=$(dscl /Local/Default -authonly $resetUser $oldPass)
+
+if [ "$passwdA" == "" ];then
+	echo "Password stored in LAPS is correct for $resetUser."
+	echo "Updating password for $resetUser."
+	/usr/local/jamf/bin/jamf resetPassword -updateLoginKeychain -username $resetUser -oldPassword "$oldPass" -password "$newPass"
+else
+	echo "Error: Password sent to script is not valid for $resetUser."
+	oldPass=""
+fi
+
+passwdB=`dscl /Local/Default -authonly $resetUser $newPass`
+
+if [ "$passwdB" == "" ];then
+	echo "New password for $resetUser is verified."
+	echo "Calling upload"
+	/usr/local/jamf/bin/jamf policy -event LAPStempPassword
+else
+	echo "Error: Password reset for $resetUser was not successful!"
+	echo "Aborting update"
+	exit 1
+fi
